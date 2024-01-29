c02ef34e2f9832aa259400dc4f5389f4b26ae5dd
Brian Morrison II <brian@brianmorrison.me>
2024-01-29 11:45:56 -0600

    trying exec

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 )
 
 func main() {
 	args := os.Args
 	if len(args) > 1 {
 		log.Printf("Hello %v!!", args[1])
-		output := os.Getenv("GITHUB_OUTPUT")
-		log.Println(output)
-		os.Setenv("GITHUB_OUTPUT", fmt.Sprintf("\noutname=%v", args[1]))
+		exec.Command("echo", fmt.Sprintf("outname=%v", args[1])).Run()
+		// os.Setenv("GITHUB_OUTPUT", fmt.Sprintf("\noutname=%v", args[1]))
 		return
 	}
 	log.Println("Hello world!!")
